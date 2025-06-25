f1bc3fd734f5a1af80dfc5b8b7c3cefbba16043e
isaac <isaacrds.dev@gmail.com>
2025-06-25 10:34:58 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Como se conectar?
+- Primeiramente, abra o terminal e entre na pasta 'connect-zap'.
+- Ainda no terminal, dê 'npm install' para instalar as dependências.
+- Também no terminal, dê 'node index' para iniciar.
+
+# Observacões
+- Quando aparecer o QR CODE, abra seu whatsapp do dispositivo móvel. Clique nos 3 pontos na parte superior
+a direita. Configuracões. Clique no qr code e escaneie o do terminal.
+- Só estará conectado se aparecer 'Tudo certo!' no terminal
+
---
+++ index.js
@@ -0,0 +1,43 @@
+import pkg from "whatsapp-web.js"
+const { Client, LocalAuth } = pkg;
+import qrcode from "qrcode-terminal"
+import cron from "node-cron" // caso queira enviar mensagens agendadas.
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
+
+client.on('qr', (qr) => { // gera o qrcode para conectar, via terminal
+    qrcode.generate(qr, { small: true });
+})
+
+client.on('message', message => { // se enviar 'oí, receberá: olá
+    if (message.body.toLowerCase() === 'oi') {
+        message.reply('Olá')
+    }
+})
+
+client.on('ready', () => { // quando se conectar, vai aparecer no console.log e chamar a function sendMessageTest
+    console.log('Tudo pronto!')
+    sendMessageTest();
+})
+
+async function sendMessageTest() { // mensagem inicial
+    try {
+        await client.sendMessage(`5511952104463@c.us`, mensagemInicial());
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function mensagemInicial() { // texto definido
+    const texto = `
+        Olá, como vai? Prazer em Conhecer-lhe. \n
+        Me chamo Isaac. Desenvolvi isto para você :)
+    `
+    return texto;
+}
+
+client.initialize()
+
+
